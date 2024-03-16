f7904d681478a9edba4dbd43d715dcc20157baa0
leonardo <leoebert19@gmail.com>
2024-03-16 00:33:00 -0300

    added image loading and rendering

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
   validates :keywords, presence: true, length: { minimum: 5, maximum: 100 }
 
+  has_many_attached :images
+
 end
